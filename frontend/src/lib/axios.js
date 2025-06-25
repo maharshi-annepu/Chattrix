aa43cb3ac782ec916e5f0325f22eddef84a12841
maharshi-annepu <144038917+maharshi-annepu@users.noreply.github.com>
2025-06-25 22:11:39 +0530

    updating axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : import.meta.env.VITE_API_URL, // Vercel or production URL from env
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
