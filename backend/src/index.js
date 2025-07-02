e1c8ec8832009ac13864d76f97e812044625ff0c
maharshi-annepu <144038917+maharshi-annepu@users.noreply.github.com>
2025-07-02 07:57:34 +0530

    Updating  index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,20 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+import express from "express";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -22,12 +22,14 @@ app.use(
     credentials: true,
   })
 );
+app.options("*", cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
-  console.log("Server is running on PORT:", PORT);
+  console.log(`Server running on port ${PORT}`);
   connectDB();
 });
